ae7cdcebcb13e39e999684fa989b1e0b269aa6fc
Emil Kampp <emil.kampp@assemblyvoting.com>
2023-03-21 10:22:37 +0100

    Skip the comment skipping the following line.. Because TS :shrug:

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,7 @@ import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import VuePopper from "@kalimahapps/vue-popper";
 
