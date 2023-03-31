d811ff4290cce2320a9d2ef45b381724cd84bf40
Emil Kampp <emil.kampp@assemblyvoting.com>
2023-03-31 11:40:26 +0200

    Improve UI when decryption has happened

--- client/src/views/BallotVerifierView.vue
+++ client/src/views/BallotVerifierView.vue
@@ -27,89 +27,49 @@ onMounted(redirectUnlessPairingCode)
 </script>
 
 <template>
-  <div class="BallotTracker" aria-flowto="tracking-code">
+  <div class="BallotVerifier">
     <CompactHeader
       :election="electionStore.election"
       :locale="localeStore.locale"
     />
 
-    <p>Pairing code: <code>{{ verificationStore.pairingCode }}</code></p>
+    <div class="BallotVerifier__Spoiled" v-if="verificationStore.ballot">
+      <h1 class="BallotVerifier__Title">Decrypted vote</h1>
 
-    <p>
-      Go to the voting system and confirm the pairing code is correctly
-      displayed there.
-    </p>
+      <pre>{{ verificationStore.ballot }}</pre>
+    </div>
 
-    <p v-if="!verificationStore.ballot">Waiting for confirmation in the voting application</p>
+    <div class="BallotVerifier__InProgress" v-else>
+      <h1 class="BallotVerifier__Title">Pairing code</h1>
 
-    <pre v-if="verificationStore.ballot">
-      {{ verificationStore.ballot }}
-    </pre>
+      <p class="BallotVerifier__Info">
+        Go to the voting application and confirm this pairing code match the one displayed there.
+      </p>
+
+      <code class="BallotVerifier__Code">{{ verificationStore.pairingCode }}</code>
+    </div>
   </div>
 </template>
 
 <style type="text/css" scoped>
-.BallotTracker {
-  font-family: "Open Sans";
-}
+  .BallotVerifier {
+    font-family: "Open Sans";
+  }
 
-.BallotTracker__Row {
-  display: flex;
-  margin-bottom: 40px;
-}
+  .BallotVerifier__Title {
+    font-size: 26px;
+    text-align: center;
+  }
 
-.BallotTracker__Row--stacked {
-  flex-direction: column;
-}
+  .BallotVerifier__Info {
+    text-align: center;
+  }
 
-.BallotTracker__TrackingCode {
-  margin-left: 40px;
-  flex-grow: 1;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-}
-
-.BallotTracker__Infobox {
-  max-width: 350px;
-}
-
-.BallotTracker__TrackingCode h3 {
-  padding: 0;
-  margin: 0;
-}
-
-.BallotTracker__TrackingCode code {
-  font-size: 40px;
-  font-family: "Red Hat Mono";
-  letter-spacing: 5px;
-}
-
-.BallotTracker__TrackingCode h3 span {
-  color: #6c757d;
-  font-weight: 600;
-  display: block;
-  margin-bottom: 20px;
-}
-
-.BallotTracker__StatusInfo {
-  width: 100%;
-}
-
-.BallotTracker__StatusInfo h3 {
-  font-size: 24px;
-}
-
-.BallotTracker__Cancel {
-  border: none;
-  background: transparent;
-  position: absolute;
-  top: 0;
-  right: 8px;
-  font-size: 40px;
-  cursor: pointer;
-  color: #adb5bd;
-}
+  .BallotVerifier__Code {
+    font-size: 40px;
+    font-family: "Red Hat Mono";
+    text-align: center;
+    display: block;
+    letter-spacing: 5px;
+  }
 </style>
