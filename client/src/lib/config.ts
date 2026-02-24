0fc991fb8ebe4a47855cdc440e6732de85235268
Martin Laursen <martin@assemblyvoting.com>
2026-02-24 12:22:19 +0100

    Switch to runtime config injection via `window.__APP_CONFIG__`

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,5 +1,13 @@
+const myWindow = window as unknown as {
+  __APP_CONFIG__: {
+    VITE_LOGO_URL: string, // Unused?
+    VITE_DBB_URL: string,
+    VITE_CONFERENCE_URL: string,
+    VITE_CONTACT_URL: string, // Unused?
+  }
+};
+
 export default {
-  logoUrl: import.meta.env.VITE_LOGO_URL,
-  dbbUrl: import.meta.env.VITE_DBB_URL || "http://localhost:3003",
-  conferenceUrl: import.meta.env.VITE_CONFERENCE_URL || "http://localhost:3016",
+  dbbUrl: myWindow.__APP_CONFIG__.VITE_DBB_URL || "http://dbb.localhost:3003",
+  conferenceUrl: myWindow.__APP_CONFIG__.VITE_CONFERENCE_URL || "http://electa.localhost:3016",
 };
