1bb308d40bfc23010ca172ff0fcecb81696e8f85
Martin Laursen <martin@assemblyvoting.com>
2026-02-24 12:35:29 +0100

    prettier?

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,13 +1,15 @@
 const myWindow = window as unknown as {
   __APP_CONFIG__: {
-    VITE_LOGO_URL: string, // Unused?
-    VITE_DBB_URL: string,
-    VITE_CONFERENCE_URL: string,
-    VITE_CONTACT_URL: string, // Unused?
-  }
+    VITE_LOGO_URL: string; // Unused?
+    VITE_DBB_URL: string;
+    VITE_CONFERENCE_URL: string;
+    VITE_CONTACT_URL: string; // Unused?
+  };
 };
 
 export default {
   dbbUrl: myWindow.__APP_CONFIG__.VITE_DBB_URL || "http://dbb.localhost:3003",
-  conferenceUrl: myWindow.__APP_CONFIG__.VITE_CONFERENCE_URL || "http://electa.localhost:3016",
+  conferenceUrl:
+    myWindow.__APP_CONFIG__.VITE_CONFERENCE_URL ||
+    "http://electa.localhost:3016",
 };
